046bfa1cea500bb4e3d943c8596bf0420b6c7575
amartincastro <castroarielm@gmail.com>
2018-08-07 10:03:22 -0700

    pass JSON object to browser

--- server.js
+++ server.js
@@ -3,5 +3,14 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Ariel',
+    likes: [
+      'Apples',
+      'Bananas'
+    ]
+  });
 });
+
+app.listen(3000);
