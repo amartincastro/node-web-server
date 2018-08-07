a66ca399e03d979bb1274e91a83f046596d98a82
amartincastro <castroarielm@gmail.com>
2018-08-07 11:05:01 -0700

    html help page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.send({
@@ -18,6 +20,11 @@ app.get('/about', (req, res) => {
   res.send('About Page');
 });
 
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error handling request'
+  });
+});
 
 // /bad - send back JSON with errorMessage property 'Error handling request'
 
