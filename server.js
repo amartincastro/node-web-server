a0372922a82f716ef3b1e3663e9fbd87a1114cbc
amartincastro <castroarielm@gmail.com>
2018-08-08 12:13:53 -0700

    middleware to take request object and return timestamp into command line

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  console.log(`${now}: ${req.method} ${req.url}`)
+  next();
+});
+
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 });
